91f9e7ad568143c3f7e41315d9dc584c5d424b78
Aditya Gianto <adityagianto@n168-p127.eduroam.kth.se>
2019-11-07 18:48:54 +0100

    fix conflict header

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import './Header.css';
+import './header.css'
+import CalendarIcon from '../img/calendar.png'
+import MenuIcon from '../img/hamburger.png'
 
 const Header = () => {
   return (
-    <div className="header-color">
-      <nav className=" container navbar navbar-expand-lg header-color">
-        <a
-          className="navbar-brand"
-          href="#"
-          style={{textDecoration: 'note', color: 'white', fontSize: '32px'}}>
-          Calendo
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </nav>
+    <div className="container-fluid">
+        <div className='row justify-content-md-center'>
+          <div className="col d-flex justify-content-center">
+          <button type="button" class="btn-primary btn-sm">
+            <img src= {MenuIcon} alt="Responsive image"></img>
+          </button>
+          </div>
+          <div className="col d-flex justify-content-center ">
+          <div className="appName"> Calendo </div>
+          </div>
+          <div className="col d-flex justify-content-center">
+          <button type="button" class="btn-secondary btn-sm">
+            <img src= {CalendarIcon} alt="Responsive image"></img>
+          </button>
+          </div>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header; 
